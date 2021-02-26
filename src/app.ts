182f71c814d567e624678ba610f1483190ea337f
LucasChlz <lucasgabrielsampaio2019@gmail.com>
2021-02-26 14:27:24 -0300

    starting database and configurations

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
+import 'reflect-metadata';
 import express from "express";
+import createConnection from './database';
 
+createConnection();
 const app = express();
 
 require('./routes/users.routes')(app)
---
+++ src/database/index.ts
@@ -0,0 +1,11 @@
+import { Connection, createConnection, getConnectionOptions } from "typeorm";
+
+export default async (): Promise<Connection> => {
+    const defaultOption = await getConnectionOptions();
+
+    return createConnection(
+        Object.assign(defaultOption, {
+            database: 'finances'
+        })
+    )
+}
